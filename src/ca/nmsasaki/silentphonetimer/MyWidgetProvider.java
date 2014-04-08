db0589989dff5e5032dd5c637622f9b064e0d4a7
Neil Sasaki <nmsasaki@hotmail.com>
2014-04-08 02:12:30 -0400

    Added Time stamps to messages so user knows when timer will expire.
    Tested that it works while phone is locked.
    
    Removed toast from expiry.

--- src/ca/nmsasaki/silentphonetimer/MyWidgetProvider.java
+++ src/ca/nmsasaki/silentphonetimer/MyWidgetProvider.java
@@ -1,5 +1,7 @@
 package ca.nmsasaki.silentphonetimer;
 
+import java.text.SimpleDateFormat;
+
 import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -7,16 +9,14 @@ import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
-//import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.SystemClock;
-//import android.content.res.Resources;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
-
-//import android.widget.RemoteViews;
+import java.text.DateFormat;
+import java.util.TimeZone;
 
 public class MyWidgetProvider extends AppWidgetProvider {
 
@@ -57,20 +57,31 @@ public class MyWidgetProvider extends AppWidgetProvider {
 				intentAlarmReceiver.setAction(Intent.ACTION_DELETE);
 				PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0, intentAlarmReceiver, 0);
 
-				final long alarmExpire = SystemClock.elapsedRealtime() + SILENT_DURATION_MILLISECONDS;
-				alarmMgr.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, alarmExpire, alarmIntent);
+				//final long alarmExpire = SystemClock.elapsedRealtime() + SILENT_DURATION_MILLISECONDS; // this uptime is ms since Jan 1, 1970.
+				final long alarmExpire = System.currentTimeMillis() + SILENT_DURATION_MILLISECONDS; // this based on system time
+				alarmMgr.set(AlarmManager.RTC_WAKEUP, alarmExpire, alarmIntent);
+				
 				// ----------------------------------------------------
 
+				DateFormat dateFormatter = DateFormat.getTimeInstance(DateFormat.SHORT);
+				// TimeZone tz = dateFormatter.getTimeZone(); - for testing timezone
+				// String dateStringTest = dateFormatter.format(SystemClock.elapsedRealtime());
+				String dateString = dateFormatter.format(alarmExpire);
+
+				// Log.i(TAG, "MyWidgetProvider::onReceive time=" + dateStringTest + ", " + dateString + ", TZ=" + tz.getDisplayName());
 				// toast
-				final String toastText = context.getString(R.string.toast_ON);
+				String toastText = context.getString(R.string.toast_ON);
+				toastText = String.format(toastText, dateString);
 				Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show();
 				
 				// Build notification ------------------------------------------------
 
-				final String notiTickerText = context.getString(R.string.notification_ON_ticker);
 				final String notiTitle = context.getString(R.string.notification_title);
-				final String notiContentText = context.getString(R.string.notification_ON_content);
 				final String notiCancel = context.getString(R.string.notification_ON_cancel);
+				//String notiTickerText = context.getString(R.string.notification_ON_ticker);
+				//notiTickerText = String.format(notiTickerText, dateString);
+				String notiContentText = context.getString(R.string.notification_ON_content);
+				notiContentText = String.format(notiContentText, dateString);
 				
 				// Pending intent to be fired when notification is clicked
 				Intent notiIntent = new Intent(context, MyWidgetProvider.class);
@@ -79,7 +90,7 @@ public class MyWidgetProvider extends AppWidgetProvider {
 				
 				// Define the Notification's expanded message and Intent:
 				Notification.Builder notificationBuilder = new Notification.Builder(context)
-						.setTicker(notiTickerText)
+						//.setTicker(notiTickerText)
 						.setSmallIcon(android.R.drawable.ic_lock_silent_mode)
 						.setContentTitle(notiTitle)
 						.setContentText(notiContentText)
@@ -94,16 +105,22 @@ public class MyWidgetProvider extends AppWidgetProvider {
 		else if (intent.getAction() == Intent.ACTION_DELETE) {
 			Log.i(TAG, "MyWidgetProvider::onReceive - ACTION_DELETE");
 			
-			// toast
-			final String toastText = context.getString(R.string.toast_OFF);
-			Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show();
-			
+//			// toast
+//			final String toastText = context.getString(R.string.toast_OFF);
+//			Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show();
+//			
 			// Build notification ------------------------------------------------
 
-			final String notiTickerText = context.getString(R.string.notification_OFF_ticker);
+			final long alarmExpired = System.currentTimeMillis();
+			DateFormat dateFormatter = DateFormat.getTimeInstance(DateFormat.SHORT);
+			String dateString = dateFormatter.format(alarmExpired);
+			
 			final String notiTitle = context.getString(R.string.notification_title);
-			final String notiContentText = context.getString(R.string.notification_OFF_content);
 			final String notiCancel = context.getString(R.string.notification_OFF_cancel);
+			String notiTickerText = context.getString(R.string.notification_OFF_ticker);
+			notiTickerText = String.format(notiTickerText, dateString);
+			String notiContentText = context.getString(R.string.notification_OFF_content);
+			notiContentText = String.format(notiContentText, dateString);
 			
 			// Pending intent to be fired when notification is clicked
 			Intent notiIntent = new Intent(context, MyWidgetProvider.class);
